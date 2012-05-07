445f7501b804d75fd3510b873c0129d4c536836c
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-07 13:30:03 +0300

    Bus year fix

--- lib/pages/categories/cars/commercial/buses_page.rb
+++ lib/pages/categories/cars/commercial/buses_page.rb
@@ -50,14 +50,12 @@ class CategoryCarsCommercialBusesPage < AdDetailsPage
   
   def get_parameter (field)
     case field
-    when "Год выпуска"
-      result = get_value_parameter(field)
     when "Пробег"
       # Пробег вводиться в тыс. км, отображается в км.
       # Делим отображаемый результат на 1000 
       result = get_value_parameter(field).to_i / 1000
     when "Новый или подержанный", "Марка", "Модель", "Тип кузова", 
-            "Тип трансмиссии", "Число мест", "Тип автобуса"
+            "Тип трансмиссии", "Число мест", "Тип автобуса", "Год выпуска"
       result = get_unique_parameter(field)
     else
       result = get_generic_parameter(field) 
