12ace9d3ffc82acc879284e3bb9bac9418507313
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-07 16:20:54 +0300

    Click All link on ad details page when it is available

--- lib/pages/ad_details_page.rb
+++ lib/pages/ad_details_page.rb
@@ -50,8 +50,8 @@ class AdDetailsPage
 
   def show_all_parameters
     # Open all params if present
-    if self.show_all_params_element.visible?
-      self.show_all_params
+    if self.show_all_params_element.when_present.visible?
+      self.show_all_params_element.when_present.click
     end
   end
 
