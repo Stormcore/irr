b390a3bb156b44dedae0cc3d09155c9eeca28251
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-17 17:45:21 +0300

    Real Estate - Rooms

--- lib/pages/categories/real-estate/rooms_rent_page.rb
+++ lib/pages/categories/real-estate/rooms_rent_page.rb
@@ -7,91 +7,23 @@ class CategoryRealEstateRoomsRentPage < AdDetailsPage
 
   @@url_suffix = "/real-estate/rooms-rent"
 
-  div :rooms, :xpath => "//div[@data-item-name='roomsForRent']"
+  irr_inline_select "Комнат сдается", "roomsForRent"
   
   span :show_kitchen_params, :text => 'Квартира'
-  text_field :meters_living_from, :name => "meters-living[from]"
-  text_field :meters_living_to, :name => "meters-living[to]"
+  irr_range_select "Площадь арендуемой комнаты", "meters-living"
   
   span :show_building_params, :text => 'Здание'
-  div :floor_house, :xpath => "//div[@data-item-name='floor_house']"
-  div :state, :xpath => "//div[@data-item-name='state']"
-  checkbox :furniture, :name => "furniture"
-  checkbox :household, :name => "household"
-  text_field :etage_from, :name => "etage-all[from]"
-  text_field :etage_to, :name => "etage-all[to]"
-  checkbox :house_lift, :name => "house-lift"
-  checkbox :gas, :name => "gas"
+  irr_multi_select "Этаж", "floor_house"
+  irr_single_select "Ремонт", "state"
+  irr_checkbox "Мебель", "furniture"
+  irr_checkbox "Бытовая техника", "household"
+  irr_range_select "Этаж в здании", "etage-all"
+  irr_checkbox "Лифты в здании", "house-lift"
+  irr_checkbox "Газ в доме", "gas"
 
   def set_parameter (hash)
-    case hash['parameter']
-    when "Комнат сдается"
-      multiselect_inline(self.rooms_element, hash['value'])
-
-    when "Площадь арендуемой комнаты"
-      unless self.meters_living_from_element.visible?
-        self.show_kitchen_params_element.parent.click
-      end
-      self.meters_living_from = hash['min']
-      self.meters_living_to = hash['max']
-
-    when "Этаж"
-      multiselect(self.floor_house_element, hash['value'])
-
-    when "Ремонт"
-      singleselect(self.state_element, hash['value'])
-
-    when "Телефон"
-      self.telephone_element.check
-    when "Мебель"
-      self.furniture_element.check
-    when "Бытовая техника"
-      self.household_element.check
-
-    when "Этаж в здании"
-      unless self.etage_from_element.visible?
-        self.show_building_params_element.parent.click
-      end
-      self.etage_from = hash['min']
-      self.etage_to = hash['max']
-
-    when "Лифты в здании"
-      unless self.house_lift_element.visible?
-        self.show_building_params_element.parent.click
-      end
-      self.house_lift_element.check
-
-    when "Газ в доме"
-      unless self.gas_element.visible?
-        self.show_building_params_element.parent.click
-      end
-      self.gas_element.check
-
-    when "Округ", "Район", "Микрорайон", "Линия метро", "Станция метро", "До метро"
-      set_metro_parameter(hash)
-
-    when "Валюта", "Срок сдачи"
-      set_rent_parameter(hash)
-
-    else
-      super(hash)
-    end
-  end
-
-  def get_parameter(field)
-    case field
-    when "Общая площадь", "Комнат сдается", 
-         "Ремонт", "Площадь арендуемой комнаты"
-      result = get_unique_parameter(field)
-
-    when "Мебель", "Бытовая техника", "Лифты в здании", "Газ в доме"
-      result = get_checkbox_parameter(field)
-
-    when "АО", "Район города", "Микрорайон", "Линия метро", "Станция метро", "До метро"
-      result = get_metro_parameter(field)
-    else
-      result = get_generic_parameter(field) 
-    end
-    result
+    self.show_kitchen_params_element.parent.click
+    self.show_building_params_element.parent.click
+    super(hash)
   end
 end
--- lib/pages/categories/real-estate/rooms_sale_page.rb
+++ lib/pages/categories/real-estate/rooms_sale_page.rb
@@ -6,80 +6,22 @@ class CategoryRealEstateRoomsSalePage < AdDetailsPage
 
   @@url_suffix = "/real-estate/rooms-sale"
 
-  div :ao, :xpath => "//div[@data-name='ab_ao']"
-  div :district, :xpath => "//div[@data-name='ab_district']"
-  div :metro_lane, :xpath => "//div[@data-name='address_metro_lane']"
-  div :metro, :xpath => "//div[@data-name='metro']"
-  text_field :distance, :name => "distance"
-  div :rooms, :xpath => "//div[@data-item-name='rooms-for-sale']"
-  text_field :meters_total_from, :name => "meters-total[from]"
-  text_field :meters_total_to, :name => "meters-total[to]"
+  irr_inline_select "Комнат в квартире", "rooms-for-sale"
+  irr_range_select "Общая площадь", "meters-total"
   
   span :show_kitchen_params, :text => 'Квартира'
-  text_field :meters_living_from, :name => "meters-living[from]"
-  text_field :meters_living_to, :name => "meters-living[to]"
-  text_field :kitchen_from, :name => "kitchen[from]"
-  text_field :kitchen_to, :name => "kitchen[to]"
+  irr_range_select "Площадь кухни", "meters-living"
   
   span :show_building_params, :text => 'Здание'
-  div :floor_house, :xpath => "//div[@data-item-name='floor_house']"
-  div :state, :xpath => "//div[@data-item-name='state']"
-  checkbox :refuse, :name => "refuse"
-  text_field :etage_from, :name => "etage-all[from]"
-  text_field :etage_to, :name => "etage-all[to]"
-  checkbox :house_lift, :name => "house-lift"
-  checkbox :gas, :name => "gas"
+  irr_multi_select "Ремонт", "state"
+  irr_checkbox "Отказ получен", "refuse"
+  irr_range_select "Этажей в здании", "floor_house"
+  irr_checkbox "Лифты в здании", "house-lift"
+  irr_checkbox "Газ в доме", "gas"
 
   def set_parameter (hash)
-    case hash['parameter']
-    when "Комнат в квартире"
-      multiselect_inline(self.rooms_element, hash['value'])
-
-    when "Общая площадь"
-      self.meters_total_from = hash['min']
-      self.meters_total_to = hash['max']
-
-    when "Ремонт"
-      singleselect(self.state_element, hash['value'])
-
-    when "Отказ получен"
-      self.refuse_element.check
-
-    when "Этажей в здании"
-      self.show_building_params_element.parent.click unless self.etage_from_element.visible?
-      self.etage_from = hash['min']
-      self.etage_to = hash['max']
-
-    when "Лифты в здании"
-      self.show_building_params_element.parent.click unless self.house_lift_element.visible?
-      self.house_lift_element.check
-
-    when "Газ в доме"
-      self.show_building_params_element.parent.click unless self.gas_element.visible?
-      self.gas_element.check
-
-    when "Округ", "Район", "Микрорайон", "Линия метро", "Станция метро", "До метро"
-      set_metro_parameter(hash)
-    else
-      super(hash)
-    end
-  end
-
-  def get_parameter(field)
-    case field
-    when "Площадь продажи", "Комнат в квартире", 
-         "Жилая площадь", "Площадь кухни", "Ремонт"
-      result = get_unique_parameter(field)
-
-    when "Отказ получен", "Лифты в здании", "Газ в доме"
-      result = get_checkbox_parameter(field)
-
-    when "Округ", "Район", "Микрорайон", "Линия метро", "Станция метро", "До метро"
-      set_metro_parameter(hash)
-
-    else
-      result = get_generic_parameter(field) 
-    end
-    result
+    self.show_kitchen_params_element.parent.click
+    self.show_building_params_element.parent.click
+    super(hash)
   end
 end
