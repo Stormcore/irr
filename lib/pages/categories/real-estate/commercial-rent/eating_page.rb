346311954d924392f9ebaced37b83b3011faf7e2
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-18 15:27:34 +0300

    Правильные контролы и район для commercial-rent/eating

--- lib/pages/categories/real-estate/commercial-rent/eating_page.rb
+++ lib/pages/categories/real-estate/commercial-rent/eating_page.rb
@@ -7,12 +7,12 @@ class CategoryRealEstateCommercialRentEatingPage < AdDetailsPage
 
   @@url_suffix = "/real-estate/commercial/eating"
 
-  irr_text_field "Общая площадь", "meters-total"
-  irr_text_field "Площадь зала", "square-hall"
+  irr_range_select "Общая площадь", "meters-total"
+  irr_range_select "Площадь зала", "square-hall"
   irr_checkbox "Отдельный вход", "entrance"
   irr_checkbox "1-я линия", "first-line"
   irr_checkbox "Оборудование", "equipment"
 
-  irr_multi_select "Система отопления", "heating"
-  irr_multi_select "Система водоснабжения", "water"
+  irr_single_select "Система отопления", "heating"
+  irr_single_select "Система водоснабжения", "water"
 end
