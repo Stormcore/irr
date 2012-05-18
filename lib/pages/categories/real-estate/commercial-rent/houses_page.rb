b06f344841402992197cfe6393434a3ddc0908d3
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-18 15:31:04 +0300

    Правильный тип контролов для CategoryRealEstateCommercialRentHousesPage

--- lib/pages/categories/real-estate/commercial-rent/houses_page.rb
+++ lib/pages/categories/real-estate/commercial-rent/houses_page.rb
@@ -7,8 +7,8 @@ class CategoryRealEstateCommercialRentHousesPage < AdDetailsPage
 
   @@url_suffix = "/real-estate/commercial/houses"
 
-  irr_text_field   "Общая площадь", "meters-total"
-  irr_multi_select "Ремонт", "state"
+  irr_range_select "Общая площадь", "meters-total"
+  irr_single_select "Ремонт", "state"
   irr_checkbox     "1-я линия", "first-line"
   irr_checkbox     "Парковка", "parking"
 end
