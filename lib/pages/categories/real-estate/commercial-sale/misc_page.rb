07592e692e9d450cce5af2d03c0539b41035ae88
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-18 14:22:33 +0300

    Commercial Sale -1 fixes

--- lib/pages/categories/real-estate/commercial-sale/misc_page.rb
+++ lib/pages/categories/real-estate/commercial-sale/misc_page.rb
@@ -6,8 +6,8 @@ class CategoryRealEstateCommercialsaleMiscPage < AdDetailsPage
 
   @@url_suffix = "/real-estate/commercial-sale/misc"
 
-  irr_text_field   "Общая площадь", "square-min"
-  irr_multi_select "Ремонт", "state"
-  irr_checkbox     "1-я линия", "first-line"
-  irr_checkbox     "Отдельный вход", "equipment"
+  irr_range_select  "Общая площадь", "square-min"
+  irr_single_select "Ремонт", "state"
+  irr_checkbox      "1-я линия", "first-line"
+  irr_checkbox      "Отдельный вход", "entrance"
 end
