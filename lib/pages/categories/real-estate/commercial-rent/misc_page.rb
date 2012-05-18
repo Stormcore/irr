838b78b71bb2bc8504d8821a4b632a90bda7574d
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-18 15:33:37 +0300

    Правильные контролы для Commercial-Rent/misc и offices

--- lib/pages/categories/real-estate/commercial-rent/misc_page.rb
+++ lib/pages/categories/real-estate/commercial-rent/misc_page.rb
@@ -7,8 +7,8 @@ class CategoryRealEstateCommercialRentMiscPage < AdDetailsPage
 
   @@url_suffix = "/real-estate/commercial/misc"
 
-  irr_text_field   "Общая площадь", "square-min"
-  irr_multi_select "Ремонт", "state"
-  irr_checkbox     "1-я линия", "first-line"
-  irr_checkbox     "Отдельный вход", "equipment"
+  irr_range_select  "Общая площадь", "square-min"
+  irr_single_select "Ремонт", "state"
+  irr_checkbox      "1-я линия", "first-line"
+  irr_checkbox      "Отдельный вход", "entrance"
 end
