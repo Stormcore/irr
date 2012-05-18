fc7cf63840807a3ed3dfd8f1341160e34237cc1d
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-18 11:47:45 +0300

    Cars - Parts fixes

--- lib/pages/categories/cars/parts/disks_page.rb
+++ lib/pages/categories/cars/parts/disks_page.rb
@@ -8,12 +8,13 @@ class CategoryCarsPartsDisksPage < AdDetailsPage
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "condition_c"
   irr_multi_select "Тип дисков", "diskstype"
-  irr_multi_select "Производитель", "producerdisks"
+  irr_multi_select "Произодитель", "producer_disks"
   irr_multi_select "Диаметр обода", "diameter_obod"
   irr_multi_select "Ширина обода", "width_disks"
   irr_multi_select "Число болтов", "bolt"
   irr_multi_select "Расстояние между болтами (PCD)", "pcd", "PCD"
   irr_multi_select "Вылет (ET)", "et" , "ET"
+  irr_multi_select "Источник", "sourcefrom"
 
   def get_parameter (field)
     case field
