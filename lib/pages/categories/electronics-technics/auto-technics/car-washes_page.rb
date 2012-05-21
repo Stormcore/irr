76d320604564145f299a432707455eeaaaabaee7
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-21 16:47:08 +0300

    woop

--- lib/pages/categories/electronics-technics/auto-technics/car-washes_page.rb
+++ lib/pages/categories/electronics-technics/auto-technics/car-washes_page.rb
@@ -10,9 +10,9 @@ class CategoryElectronicsTechnicsAutoTechnicsCarWashesPage < AdDetailsPage
   irr_multi_select "Состояние", "used-or-new"
   irr_multi_select "Марка", "make"
   irr_single_select "Класс", "klass"
-  irr_range_select "Производительность", "proizvoditelnost"
-  irr_range_select "Давление", "davlenie"
+  irr_range_select "Производительность, л\ч", "proizvoditelnost", "Производительность"
+  irr_range_select "Давление, бар", "davlenie", "Давление"
   irr_single_select "Материал корпуса насоса", "material_korpusa_nasosa"
-  irr_single_select "Насадки", "nasadki"
+  irr_multi_select "Насадки", "nasadki"
   irr_checkbox     "Использование моющего средства", "ispolzovanie_mojushchego_sredstva"
 end
