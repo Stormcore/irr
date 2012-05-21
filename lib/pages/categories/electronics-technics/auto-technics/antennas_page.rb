f8373ac1a8034e1541f56759bfc75a860896a4eb
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-21 16:35:06 +0300

    Правильное имя класса для антенн

--- lib/pages/categories/electronics-technics/auto-technics/antennas_page.rb
+++ lib/pages/categories/electronics-technics/auto-technics/antennas_page.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class CategoryElectronicsTechnicsAutoTechnicsTVPage < AdDetailsPage
+class CategoryElectronicsTechnicsAutoTechnicsAntennasPage < AdDetailsPage
   include PageObject
  
   @@url_suffix = "/electronics-technics/auto-technics/antennas"
