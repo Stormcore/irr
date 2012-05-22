f9ec8e049e32ef0b44bad3b313e9bb96501ffad2
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-22 10:57:46 +0300

    Удаляем тег @wip  с автотехники

--- lib/pages/categories/electronics-technics/auto-technics/radar-detector_page.rb
+++ lib/pages/categories/electronics-technics/auto-technics/radar-detector_page.rb
@@ -8,8 +8,8 @@ class CategoryElectronicsTechnicsAutoTechnicsRadarDetectorPage < AdDetailsPage
   
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
-  irr_multi_select "Диапазоны", "make"
-  irr_multi_select "Марка", "diapazoni"
+  irr_multi_select "Диапазоны", "diapazoni"
+  irr_multi_select "Марка", "make"
   irr_single_select "Режимы", "rejimi"
   irr_checkbox      "Детектор лазера", "detector_lazera"
   irr_checkbox      "Пеленг лазера 360 гр.", "peleng_lazera"
