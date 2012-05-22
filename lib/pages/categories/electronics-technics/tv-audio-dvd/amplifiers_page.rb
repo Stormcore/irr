59fc129a10a99cd79fa0c705a635f2bb0ef50ce0
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-22 12:55:09 +0300

    Электроника и техника -> Аудио, Видео -> Акустические системы

--- lib/pages/categories/electronics-technics/tv-audio-dvd/amplifiers_page.rb
+++ lib/pages/categories/electronics-technics/tv-audio-dvd/amplifiers_page.rb
@@ -4,6 +4,7 @@ class CategoryElectronicsTechnicsTVAudioDVDAmplifiersPage < AdDetailsPage
   include PageObject
  
   @@url_suffix = "/electronics-technics/tv-audio-dvd/amplifiers"
+  @category = "Электроника и техника -> Аудио, Видео -> Усилители"
   
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
