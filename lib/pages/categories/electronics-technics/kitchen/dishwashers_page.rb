936dc0474377d79edcbebfd964855cce2f6d6bb7
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-22 11:32:30 +0300

    Электроника и техника -> Кухонная техника -> Посудомоечные машины

--- lib/pages/categories/electronics-technics/kitchen/dishwashers_page.rb
+++ lib/pages/categories/electronics-technics/kitchen/dishwashers_page.rb
@@ -10,6 +10,6 @@ class CategoryElectronicsTechnicsKitchenDishwashersPage < AdDetailsPage
   irr_multi_select "Состояние", "used-or-new"
   irr_multi_select "Марка", "make"
   irr_checkbox     "Встраиваемая техника", "fridge_builtin"
-  irr_single_select "Объем загрузки", "washmach_vol"
+  irr_single_select "Объем загрузки, комплектов", "washmach_vol"
   irr_checkbox     "Сушка", "washmach_dry"
 end
