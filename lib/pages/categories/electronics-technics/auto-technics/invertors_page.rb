6081d6954ab5c05ec192d598c6836a5792643602
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-22 10:01:41 +0300

    Auto-technics 3

--- lib/pages/categories/electronics-technics/auto-technics/invertors_page.rb
+++ lib/pages/categories/electronics-technics/auto-technics/invertors_page.rb
@@ -8,9 +8,9 @@ class CategoryElectronicsTechnicsAutoTechnicsInvertorsPage < AdDetailsPage
   
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
-  irr_range_select "Выходное напряжение", "vihodnoe_napryajenije"
-  irr_range_select "Выходная мощность", "vihodnaya_moshnost"
-  irr_range_select "Пиковая мощность", "cf_labelpikovaya_moshnost"
+  irr_range_select "Выходное напряжение, В", "vihodnoe_napryajenije", "Выходное напряжение"
+  irr_range_select "Выходная мощность, Вт", "vihodnaya_moshnost", "Выходная мощность"
+  irr_range_select "Пиковая мощность, Вт", "cf_labelpikovaya_moshnost", "Пиковая мощность"
   irr_range_select "КПД", "kpd"
   irr_checkbox     "Защита от перегрузки", "zashita_ot_peregruzki"
   irr_checkbox     "USB-порт", "usb_port"
