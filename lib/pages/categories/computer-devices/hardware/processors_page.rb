ae17359c073931ca278df9029fb432337f8d61c5
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-23 16:53:10 +0300

    woop

---
+++ lib/pages/categories/computer-devices/hardware/processors_page.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+class CategoryComputerHardwareProcessorsPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/computers-devices/hardware/processors"
+  @category = "Компьютерная техника -> Комплектующие -> Процессоры"
+
+  irr_multi_select "Тип предложения", "offertype"
+  irr_multi_select "Состояние", "used-or-new"
+  irr_multi_select "Производитель", "make"
+  irr_range_select "Частота, МГц", "frequency"
+end
