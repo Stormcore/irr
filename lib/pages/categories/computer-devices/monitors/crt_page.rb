aeeb3b7c0a96b10085acdbce7b9bb438a751fca0
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-23 14:12:17 +0300

    Monitors Printers

---
+++ lib/pages/categories/computer-devices/monitors/crt_page.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+class CategoryComputerDevicesMonitorsCRTPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/computers-devices/monitors/lcd"
+  @category = "Компьютерная техника -> Мониторы -> ЭЛТ"
+
+  irr_multi_select "Тип предложения", "offertype"
+  irr_multi_select "Состояние", "used-or-new"
+  irr_multi_select "Марка", "make"
+  irr_range_select "Диагональ экрана, дюймы", "diagonal"
+
+end
---
+++ lib/pages/categories/computer-devices/monitors/lcd_page.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+class CategoryComputerDevicesMonitorsLCDPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/computers-devices/monitors/lcd"
+  @category = "Компьютерная техника -> Мониторы -> LCD (ЖК)"
+
+  irr_multi_select "Тип предложения", "offertype"
+  irr_multi_select "Состояние", "used-or-new"
+  irr_multi_select "Марка", "make"
+  irr_range_select "Диагональ экрана, дюймы", "diagonal"
+
+end
