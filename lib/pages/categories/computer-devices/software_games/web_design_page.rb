775e799517b8bf445e1dfaa08b293d2bc7c629bd
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-24 12:22:36 +0300

    Web-design

--- lib/pages/categories/computer-devices/software_games/web_design_page.rb
+++ lib/pages/categories/computer-devices/software_games/web_design_page.rb
@@ -3,7 +3,7 @@
 class CategoryComputerSoftwareWebDesignPage < AdDetailsPage
   include PageObject
 
-  @@url_suffix = "/computers-devices/software_games/web-design"
+  @@url_suffix = "/computers-devices/software_games/web_design"
   @category = "Компьютерная техника -> Программное обеспечение и игры -> Веб-дизайн"
 
   irr_multi_select "Тип предложения", "offertype"
