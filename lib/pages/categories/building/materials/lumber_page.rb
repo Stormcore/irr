3af66439532ba1180423e54ea0ce2d69ae038777
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-25 15:42:20 +0300

    Building - Materials fixes

--- lib/pages/categories/building/materials/lumber_page.rb
+++ lib/pages/categories/building/materials/lumber_page.rb
@@ -9,7 +9,16 @@ class CategoryBuildingMaterialsLumberPage < AdDetailsPage
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
   irr_multi_select "Тип пиломатериала", "woodmaterial_type"
-  irr_range_select "Высота, мм", "woodmaterial_height"
-  irr_range_select "Ширина, мм", "woodmaterial_width"
-  irr_range_select "Длина, мм", "woodmaterial_length"
+  irr_range_select "Высота", "woodmaterial_height"
+  irr_range_select "Ширина", "woodmaterial_width"
+  irr_range_select "Длина", "woodmaterial_length"
+
+  def get_parameter(field)
+    case field
+    when "Высота", "Ширина", "Длина"
+      super(field).gsub(/ мм/,'')
+    else
+      super(field)
+    end
+  end
 end
