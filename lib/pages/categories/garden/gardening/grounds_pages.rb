0b6a0902058dc2957daf648eefe26a0d398bd6af
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-28 16:33:52 +0300

    Все для дачи -> Товары для садоводства

---
+++ lib/pages/categories/garden/gardening/grounds_pages.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+class CategoryGardenGardeningGroundsPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/garden/gardening/grounds"
+  @category = "Все для дачи -> Товары для садоводства -> Грунт, удобрения"
+
+  irr_multi_select "Тип предложения", "offertype"
+  irr_multi_select "Состояние", "used-or-new"
+end
