67383addb67f26aef9eb6d0382804426acf42a4a
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-28 10:20:48 +0300

    Bulding - Other

--- lib/pages/categories/building/constructions_page.rb
+++ lib/pages/categories/building/constructions_page.rb
@@ -9,9 +9,18 @@ class CategoryBuildingConstructionsPage < AdDetailsPage
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
   irr_multi_select "Тип конструкции", "type"
-  irr_multi_select "Порода дерева", "wood_species"
+  irr_single_select "Порода дерева", "wood_species"
   irr_single_select "Материал", "construction_material"
   irr_checkbox     "Под ключ", "turn-key"
-  irr_range_select "Ширина, мм", "construction_width"
-  irr_range_select "Длина, мм", "construction_length"
+  irr_range_select "Ширина", "construction_width"
+  irr_range_select "Длина", "construction_length"
+
+  def get_parameter(field)
+    case field
+    when "Ширина", "Длина"
+      super(field).gsub(/ мм/,'')
+    else
+      super(field)
+    end
+  end
 end
