0cc8230f76dd16218268e0e66fcde7389024d388
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-31 14:29:46 +0300

    Тип сантехники

--- lib/pages/categories/building/plumbing/other_page.rb
+++ lib/pages/categories/building/plumbing/other_page.rb
@@ -8,4 +8,5 @@ class CategoryBuildingPlumbingOtherPage < AdDetailsPage
 
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
+  irr_multi_select "Тип сантехники", "type"
 end
