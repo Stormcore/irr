4d5986139ae2146beb93f4178ddc0334a91644d8
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-05-31 10:10:47 +0300

    Правильный ID для поля "Номинальная мощность"

--- lib/pages/categories/electronics-technics/auto-technics/acoustics_page.rb
+++ lib/pages/categories/electronics-technics/auto-technics/acoustics_page.rb
@@ -12,6 +12,6 @@ class CategoryElectronicsTechnicsAutoTechnicsAcousticsPage < AdDetailsPage
   irr_single_select "Тип", "autoacoustic_type"
   irr_single_select "Типоразмер", "tiporazmer"
   irr_range_select "Количество полос", "kolichestvo_polos"
-  irr_range_select "Номинальная мощность", "nominalnaya_moshnost"
+  irr_range_select "Номинальная мощность", "nominalnaya_moshnos"
   irr_range_select "Импеданс, Ом", "impedans", "Импеданс"
 end
