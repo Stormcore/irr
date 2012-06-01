d0f6d5c56be852c220d5a2e54ca14a23037a684a
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-01 13:36:10 +0300

    Бизнес и партнерство -> Бизнес услуги

--- lib/pages/categories/business/business-services/credit_page.rb
+++ lib/pages/categories/business/business-services/credit_page.rb
@@ -3,7 +3,7 @@
 class CategoryBusinessBusinessServicesCreditPage < AdDetailsPage
   include PageObject
 
-  @@url_suffix = "/business/business-services/legal"
+  @@url_suffix = "/business/business-services/credit"
   @category = "Бизнес и партнерство -> Бизнес услуги -> Помощь в получении кредита"
 
   irr_multi_select "Тип предложения", "offertype"
