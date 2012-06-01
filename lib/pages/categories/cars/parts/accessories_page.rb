ef889ffd3da5b06be8f503ccb33cd94e5ea5fd4b
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-01 12:47:50 +0300

    Updated webdriver and cars/parts/accessories

--- lib/pages/categories/cars/parts/accessories_page.rb
+++ lib/pages/categories/cars/parts/accessories_page.rb
@@ -6,6 +6,4 @@ class CategoryCarsPartsAccessoriesPage < AdDetailsPage
   @@url_suffix = "/cars/parts/accessories"
   @category = "Авто и мото -> Автозапчасти и принадлежности -> Аксессуары"
 
-  irr_multi_select "Источник", "sourcefrom"
-
 end
