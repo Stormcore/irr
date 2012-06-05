6d3a2e781af634ad52f489d07aca74a844ffbe86
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-05 11:04:03 +0300

    Правильный inline select

--- lib/controls/inline_select_control.rb
+++ lib/controls/inline_select_control.rb
@@ -9,7 +9,7 @@ def irr_inline_select(getter_name, identifier, setter_name = nil)
       self.show_all_params
       Watir::Wait.until {self.all_params_element.style('display') == "table"}
     end
-  
+
     xpath = "//table[@id='allParams']/tbody/tr[./th/span[text()='#{getter_name}']]/td"
     begin
       self.cell_element(:xpath => xpath).when_present(10).text
@@ -22,11 +22,8 @@ def irr_inline_select(getter_name, identifier, setter_name = nil)
   define_method("#{function_name}=") do |hash|
     begin
       self.expand_all_parameters
-      element = self.div_element(:xpath => "//div[@data-item-name='#{identifier}']")
-      element.visible?
       hash['value'].split(",").each do |value|
-        element.link_element(:xpath => "//a[./span[text()='#{value}']]").
-              when_present.click
+        self.link_element(:xpath => "//div[@data-item-name='#{identifier}']//a[./span[text()='#{value}']]").when_present.click
       end
     rescue Exception => e
       raise "Ошибка в поле #{getter_name} (id '#{identifier}')\n#{e}"
