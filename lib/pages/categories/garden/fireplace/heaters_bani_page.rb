391a00ceb54531950fbaeedd8a323d4bb322ca91
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-06 10:50:18 +0300

    Все для дачи -> Камины, печи фиксы

--- lib/pages/categories/garden/fireplace/heaters_bani_page.rb
+++ lib/pages/categories/garden/fireplace/heaters_bani_page.rb
@@ -10,7 +10,7 @@ class CategoryGardenFireplaceHeatersBaniPage < AdDetailsPage
   irr_multi_select "Состояние", "used-or-new"
   irr_single_select "Вид", "type"
   irr_range_select "Вес камней, кг", "bani_rock_weight"
-  irr_range_select "Диаметр дымохода, см", "chimney_size"
+  irr_range_select "Диаметр дымохода, мм", "chimney_size"
   irr_range_select "Высота, мм", "height"
   irr_range_select "Глубина, мм", "depth"
   irr_range_select "Ширина, мм", "width"
