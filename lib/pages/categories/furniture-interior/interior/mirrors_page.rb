e84caf35be0464b20c4d2151b08f4f3dc3d869fc
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-06 10:01:59 +0300

    Фиксы Мебель, интерьер, обиход -> Интерьер

--- lib/pages/categories/furniture-interior/interior/mirrors_page.rb
+++ lib/pages/categories/furniture-interior/interior/mirrors_page.rb
@@ -8,4 +8,7 @@ class CategoryFurnitureInteriorInteriorMirrorsPage < AdDetailsPage
 
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
+  irr_single_select "Форма", "type"
+  irr_range_select "Длина, см", "length"
+  irr_range_select "Ширина, см", "width"
 end
