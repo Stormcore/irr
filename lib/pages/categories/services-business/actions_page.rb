bedd002721094a5558067e0977b90027ac0a10fc
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-07 15:52:58 +0300

    Услуги и деятельность

---
+++ lib/pages/categories/services-business/actions_page.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+class CategoryServicesBusinessActionsPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/services-business/actions"
+  @category = "Услуги и деятельность -> Организация праздников, фото-, видеосъёмка"
+
+  irr_multi_select "Тип предложения", "offertype"
+end
+
---
+++ lib/pages/categories/services-business/other_page.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+class CategoryServicesBusinessOtherPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/services-business/other"
+  @category = "Услуги и деятельность -> Другое"
+
+  irr_multi_select "Тип предложения", "offertype"
+end
+
