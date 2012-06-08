d82a0cc65104042987e276a15cdd44a26dfaa109
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-08 11:14:46 +0300

    Услуги и деятельность -> Транспортные услуги и аренда -> Такси, аренда и прокат, пассажирские перевозки

--- lib/pages/categories/services-business/transportation/hiring_page.rb
+++ lib/pages/categories/services-business/transportation/hiring_page.rb
@@ -6,7 +6,7 @@ class CategoryServicesBusinessTransportationHiringPage < AdDetailsPage
   @@url_suffix = "/services-business/transportation/hiring"
   @category = "Услуги и деятельность -> Транспортные услуги и аренда -> Такси, аренда и прокат, пассажирские перевозки"
 
-  irr_multi_select "Тип предложения", "offer"
+  irr_multi_select "Тип предложения", "offertype"
   irr_single_select "Вид транспорта", "auto_type"
   irr_single_select "Назначение", "use_type"
   irr_single_select "С водителем", "driver_type"
