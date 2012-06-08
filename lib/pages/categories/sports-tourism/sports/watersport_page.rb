dd905826106b59a1dd9c7fd760ec7ee9a7ba8e77
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-08 11:19:54 +0300

    Спорт, туризм, отдых -> Спортивные игры, виды спорта - правильные ID полей

--- lib/pages/categories/sports-tourism/sports/watersport_page.rb
+++ lib/pages/categories/sports-tourism/sports/watersport_page.rb
@@ -6,8 +6,8 @@ class CategorySportsTourismSportsWatersportsPage < AdDetailsPage
   @@url_suffix = "/sports-tourism/sports/watersport"
   @category = "Спорт, туризм, отдых -> Спортивные игры, виды спорта -> Водный спорт, дайвинг"
 
-  irr_multi_select "Тип предложения", "offer"
-  irr_multi_select "Состояние", "used-or-new"
+  irr_multi_select "Тип предложения", "offertype"
+  irr_multi_select "Состояние", "type"
   irr_single_select "Тип снаряжения", "watersport_equip"
 end
 
