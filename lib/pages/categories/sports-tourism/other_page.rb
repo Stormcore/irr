35851d968b776b169925670f1adc768ffb8f2cc9
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-08 11:16:06 +0300

    Спорт, туризм, отдых -> Другое - правильные ID полей

--- lib/pages/categories/sports-tourism/other_page.rb
+++ lib/pages/categories/sports-tourism/other_page.rb
@@ -6,7 +6,7 @@ class CategorySportsTourismOtherPage < AdDetailsPage
   @@url_suffix = "/sports-tourism/other"
   @category = "Спорт, туризм, отдых -> Другое"
 
-  irr_multi_select "Тип предложения", "offer"
-  irr_multi_select "Состояние", "used-or-new"
+  irr_multi_select "Тип предложения", "offertype"
+  irr_multi_select "Состояние", "type"
 end
 
