4766379b32129c16e65019fe6c19e1becb5e1f61
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-08 11:09:26 +0300

    Услуги и деятельность -> Услуги для животных: правильный ID для типа предлжения и цены

--- lib/pages/categories/services-business/animals/grooming_page.rb
+++ lib/pages/categories/services-business/animals/grooming_page.rb
@@ -6,6 +6,6 @@ class CategoryServicesBusinessAnimalsGroomingPage < AdDetailsPage
   @@url_suffix = "/services-business/animals/grooming"
   @category = "Услуги и деятельность -> Услуги для животных -> Стрижка"
 
-  irr_multi_select "Тип предложения", "offer"
+  irr_multi_select "Тип предложения", "offertype"
 end
 
