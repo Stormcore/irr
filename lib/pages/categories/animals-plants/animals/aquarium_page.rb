b01758d837d5502666453d70bb3f263a7a094dd2
Vadim Rutkovsky <vrutkovsky@prontosoft.by>
2012-06-12 17:32:35 +0300

    Животные и растения -> Животные: правильный выбор возраста

--- lib/pages/categories/animals-plants/animals/aquarium_page.rb
+++ lib/pages/categories/animals-plants/animals/aquarium_page.rb
@@ -8,7 +8,17 @@ class CategoryAnimalsPlantsAnimalsAquariumPage < AdDetailsPage
 
   irr_multi_select "Тип предложения", "offertype"
   irr_range_select "Возраст", "age"
-  irr_single_select "Лет/мес.", "age"
+  div :age_select, :xpath => "//div[@data-item-name='age']"
+
+  def set_parameter(hash)
+    if hash['parameter'] == 'Лет/мес.'
+      self.age_select_element.when_present.visible?
+      self.age_select_element.div_element(:class => "controlSelectS").when_present(10).click
+      self.age_select_element.element.div(:text => hash['value'].strip).when_present(10).click
+    else
+      super(hash)
+    end
+  end
 
   def get_parameter(field)
     case field
--- lib/pages/categories/animals-plants/animals/dogs_page.rb
+++ lib/pages/categories/animals-plants/animals/dogs_page.rb
@@ -9,8 +9,18 @@ class CategoryAnimalsPlantsAnimalsDogsPage < AdDetailsPage
   irr_multi_select "Тип предложения", "offertype"
   irr_single_select "Порода", "breed"
   irr_range_select "Возраст", "age"
-  irr_single_select "Лет/мес.", "age"
   irr_single_select "Пол", "animal_sex"
+  div :age_select, :xpath => "//div[@data-item-name='age']"
+
+  def set_parameter(hash)
+    if hash['parameter'] == 'Лет/мес.'
+      self.age_select_element.when_present.visible?
+      self.age_select_element.div_element(:class => "controlSelectS").when_present(10).click
+      self.age_select_element.element.div(:text => hash['value'].strip).when_present(10).click
+    else
+      super(hash)
+    end
+  end
 
   def get_parameter(field)
     case field
