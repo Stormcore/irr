fbe27760503b9111dedec3c9b934a028c2221628
Vadim Rutkovsky <roignac@gmail.com>
2012-07-24 18:11:13 +0300

    Дополнительные ветви для проверки условий "Большеи" и "меньше"

--- features/step_definitions/category_pages_steps.rb
+++ features/step_definitions/category_pages_steps.rb
@@ -103,6 +103,10 @@ end
         actual_value.to_i.should be <= expected_array[1].to_i
       when "содержит"
         actual_value.should include(expected)
+      when "больше"
+        actual_value.to_i.should be > expected.to_i
+      when "меньше"
+        actual_value.to_i.should be < expected.to_i
       else
         eval("actual_value.to_i.should be #{operator} expected.to_i")
       end
