aee6c3ec437b0a2d0f676151ad1f01f5a3120532
Vadim Rutkovsky <roignac@gmail.com>
2012-08-02 16:48:47 +0300

    Оборудование -> Строительное оборудование - правильное имя класса и название поля

--- lib/pages/categories/equipment/building_page.rb
+++ lib/pages/categories/equipment/building_page.rb
@@ -4,9 +4,9 @@ class CategoryEquipmentBuildingPage < AdDetailsPage
   include PageObject
 
   @@url_suffix = "/equipment/building"
-  @category = "Оборудование -> Строительное и ремонтное"
+  @category = "Оборудование -> Строительное оборудование"
 
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
-  irr_multi_select "Тип оборудования", "type"
+  irr_multi_select "Тип оборудования", "advert_type_id"
 end
