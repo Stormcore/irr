12bf4bb033cfd777dca89e8451d824f87382c93f
Vadim Rutkovsky <roignac@gmail.com>
2012-08-07 17:17:15 +0300

    Товары для детей -> Коляски - правильное название класса

--- lib/pages/categories/children/baby-carriage_page.rb
+++ lib/pages/categories/children/baby-carriage_page.rb
@@ -4,7 +4,7 @@ class CategoryChildrenBabyCarriagePage < AdDetailsPage
   include PageObject
 
   @@url_suffix = "/children/baby-carriage"
-  @category = "Товары для детей -> Детские коляски"
+  @category = "Товары для детей -> Коляски"
 
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
