095a740763f7cef914767800dd37598dacc7559b
Vadim Rutkovsky <roignac@gmail.com>
2012-08-16 16:51:00 +0300

    Услуги и деятельность -> Организация праздников, фото-, видеосъёмка

--- lib/pages/categories/services-business/actions_page.rb
+++ lib/pages/categories/services-business/actions_page.rb
@@ -7,5 +7,6 @@ class CategoryServicesBusinessActionsPage < AdDetailsPage
   @category = "Услуги и деятельность -> Организация праздников, фото-, видеосъёмка"
 
   irr_multi_select "Тип предложения", "offer"
+  irr_multi_select "Вид услуги", "service_type"
 end
 
