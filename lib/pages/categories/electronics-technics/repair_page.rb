f1fe51392f43758492d81ca3275c3c01a4693651
Vadim Rutkovsky <roignac@gmail.com>
2012-08-16 15:50:38 +0300

    Правильное название для категории "Ремонт"

--- lib/pages/categories/electronics-technics/repair_page.rb
+++ lib/pages/categories/electronics-technics/repair_page.rb
@@ -4,7 +4,7 @@ class CategoryElectronicsTechnicsRepairPage < AdDetailsPage
   include PageObject
 
   @@url_suffix = "/electronics-technics/repair"
-  @category = "Электроника и техника -> Ремонт и сервис"
+  @category = "Электроника и техника -> Ремонт"
 
   irr_multi_select "Тип предложения", "offertype"
 end
