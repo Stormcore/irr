aed1225c35c68e283138b99f4c36864182376fb6
Vadim Rutkovsky <roignac@gmail.com>
2012-08-27 17:26:13 +0300

    Правильные названия категорий

--- lib/pages/categories/real-estate/out-of-town/lands_page.rb
+++ lib/pages/categories/real-estate/out-of-town/lands_page.rb
@@ -5,7 +5,7 @@ class CategoryRealEstateOutoftownLandsPage < AdDetailsPage
   include CityWithMetro
 
   @@url_suffix = "/real-estate/out-of-town/lands"
-  @category = "Недвижимость -> Дома, дачи и участки. Продажа -> Участки"
+  @category = "Недвижимость -> Дома, дачи, участки. Продажа -> Участки"
 
   irr_single_select  "Категория земли", "land_purpose"
   irr_single_select  "Вид разрешенного использования", "land_usage"
