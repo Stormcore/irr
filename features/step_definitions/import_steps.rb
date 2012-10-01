4af255a080003e0076759f322e23d7b6ed23e747
Vadim Rutkovsky <roignac@gmail.com>
2012-10-02 01:01:59 +0300

    Update features/step_definitions/import_steps.rb

--- features/step_definitions/import_steps.rb
+++ features/step_definitions/import_steps.rb
@@ -32,9 +32,10 @@
 
     # Переходим в каталог с кастомимпортом, Запускаем импорт и читаем лог
     import_command = 
-          "cd #{ssh_data['path']} && ./custom_import_test.php autoimport.xml && " +
+          "cd #{ssh_data['path']} && ls -l && ./custom_import_test.php autoimport.xml && " +
 #          'awk \'/^importing autoimport.xml/ { buf = "" } { buf = buf "\n" $0 } END { print buf }\' import.log'
           "tail import.log"
+    puts import_command
     import_output = ssh.exec!(import_command).to_s.force_encoding("UTF-8")
     puts "Лог импорта: <pre style='white-space:pre-wrap;'>#{import_output}</pre>"
   end
