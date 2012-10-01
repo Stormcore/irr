01d03da415b9499a369227b5b6e6d54178f1e10d
Vadim Rutkovsky <roignac@gmail.com>
2012-10-01 12:57:19 +0300

    Очищаем файлы для trucklist.ru

--- lib/pages/categories/contacts-announces/other_page.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-class CategoryContactsAnnouncesOtherPage < AdDetailsPage
-  include PageObject
-
-  @@url_suffix = "/contacts-announces/other"
-  @category = "Знакомства и общение -> Другое"
-end
