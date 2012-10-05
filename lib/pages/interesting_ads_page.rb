e7366ae54090878dc827eae648693186c83e4383
Vadim Rutkovsky <roignac@gmail.com>
2012-10-05 18:04:33 +0300

    Правильно получаем ID объявления в Интересных объявлениях

--- lib/pages/interesting_ads_page.rb
+++ lib/pages/interesting_ads_page.rb
@@ -16,7 +16,7 @@ class InterestingAd
     @price = element.paragraph_element(class: "prise").text
     @photo = element.image_element.attribute("src")
     @url = element.element.link.href
-    @id = @url.match(/advert(\d+).html/).to_s
+    @id = @url[/advert(\d+).html/, 1]
   end
 
   def get_photo
