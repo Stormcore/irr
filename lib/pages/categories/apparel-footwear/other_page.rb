d29d1c1e418f7c269270e2f856865ded68123b4c
Vadim Rutkovsky <roignac@gmail.com>
2012-10-16 10:12:52 +0300

    Неконечные рубрики - Одежда, обувь, аксессуары

--- lib/pages/categories/apparel-footwear/other_page.rb
+++ lib/pages/categories/apparel-footwear/other_page.rb
@@ -10,3 +10,26 @@ class CategoryApparelFootwearOtherPage < AdDetailsPage
   irr_multi_select "Состояние", "used-or-new"
 end
 
+class CategoryApparelFootwearAccessoriesPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/apparel-footwear/accessories"
+  @category = "Одежда, обувь, аксессуары -> Аксессуары"
+
+end
+
+class CategoryApparelFootwearJewelleryBijouPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/apparel-footwear/jewellery-bijou"
+  @category = "Одежда, обувь, аксессуары -> Украшения, ювелирные изделия, бижутерия"
+
+end
+
+class CategoryApparelFootwearWeddingPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/apparel-footwear/wedding"
+  @category = "Одежда, обувь, аксессуары -> Всё для свадьбы"
+
+end
